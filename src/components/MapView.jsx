722d810ed371ea73914f9e413298c6523b149388
Vanshika218 <vinitaagarwal218@gmail.com>
2025-09-14 17:25:18 +0530

    Update MapView.jsx

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -18,7 +18,7 @@ export default function MapView() {
   }, []);
 
   return (
-    <MapContainer center={[28.61, 77.23]} zoom={12} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[28.61, 77.23]} zoom={12} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="© OpenStreetMap"
